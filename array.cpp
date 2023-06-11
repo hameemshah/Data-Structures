aa5a2d3570e6c77a174ca34b8096c2e3141b0545
LetMeCodeCInCPP <135963490+LetMeCodeCInCPP@users.noreply.github.com>
2023-06-11 13:08:51 +0000

    Update array.cpp

--- array.cpp
+++ array.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
-using namespace std;
-#define MAX_SIZE 100
+// using namespace std; Please, don't use namespace std.
 int main(void) {
-
-    int arr[MAX_SIZE];
     int n;
-    cout << "Enter the number of elements of array : " << endl;
-    cin >> n;
-    for(int i = 0; i < n; i++)
-        cin >> arr[i];
-    
-    for(int i = 0; i < n; i++)
-        cout << arr[i] << " ";
+    std::cout << "Enter the number of elements of array : " << std::endl;
+    std::cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        std::cin >> arr[i];
+    }
+    for(int i = 0; i < n; i++) {
+        std::cout << arr[i] << ' '; // Use char instead of string.
+    }
+    std::cout << std::endl;
     return 0;
 }
